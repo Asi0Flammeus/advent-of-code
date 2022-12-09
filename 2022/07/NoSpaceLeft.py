6d32a13e7316a103a8f56972a56789c9015aa652
asi0 <asi0.flammeus@pm.me>
2022-12-09 22:58:07 +0100

    2022/07 part II solved

--- 2022/07/NoSpaceLeft.py
+++ 2022/07/NoSpaceLeft.py
@@ -5,12 +5,14 @@ from filesystem import File, Folder
 
 start_time = time.time()
 
-# Set the actual_name_folder to the root folder and create the root folder
+""" Recreate the root folder by reading input.txt line by line
+"""
+# Set the actual folder to the root
 root_folder = Folder("/", None)
 actual_folder = copy.deepcopy(root_folder)
 actual_folder = root_folder
 all_folders = []
-# Recreate the root folder by reading input.txt line by line
+
 with open("input.txt") as f:
     next(f)
     for line in f:
@@ -39,15 +41,27 @@ with open("input.txt") as f:
                 size = int(parts[0])
                 actual_folder.add_file(name,size)
 
-# Compute the sum of all root folders with a size lower than 100000
+""" Compute the sum of all root folders with a size lower than 100000
+    and also the candidate folder size for allowing the update
+"""
 total_size = 0
+DISK_SPACE = 70000000
+NEEDED_SPACE = 30000000
+DISK_SPACE_USED = root_folder.give_total_size()
+candidate_folder_size = root_folder.give_total_size()
+
 for folder in all_folders:
+    # first part of the puzzle
     if folder.give_total_size() < 100000:
         total_size += folder.give_total_size()
+    # second part of the puzzle
+    if DISK_SPACE - (DISK_SPACE_USED - folder.give_total_size()) > NEEDED_SPACE:
+        candidate_folder_size = min(candidate_folder_size,folder.give_total_size())
 
 """ pull out answers and script performance
 """
 print("the first answer is",total_size)
+print("the second answer is",candidate_folder_size)
 # calculate the memory usage
 used = psutil.Process().memory_info().rss / 1024 / 1024
 # print the memory usage, rounded to two decimal places
--- 2022/07/README.md
+++ 2022/07/README.md
@@ -2,10 +2,14 @@
 
 During the journey through the jungle, I notice that my communication system is buggy. As a first reflex, I wanted to update that damn sytem but it pull out a daunting error `no space left on device`...
 
-Hence the quest of today: how can I make some space for the update?
+Hence the 1st quest of today: how can I make some space for the update?
 
 After parsing the filesystem of the device, I end up with a history of the terminal ouput (ie. the input).
 Based on this history I must compute the size of each folder and determine the total size of all folders with a size lower than 100,000. 
+
+Knowing that the total dispace available to the filesystem is 70,000,000 and that I need at least 30,000,000 to update, I must find the smallest directory that when deleting would free up enough space on the filesystem. 
+
+Hence the 2nd quest of the day: what is the total size of such folder?
 
 ### INPUT DESCRIPTION 
 
@@ -46,3 +50,4 @@ Based on this history I must compute the size of each folder and determine the t
             - if the first is dir, create a new folder
             - else, create a new file
 - compute the sum of all folders with a size lower than 100000
+
