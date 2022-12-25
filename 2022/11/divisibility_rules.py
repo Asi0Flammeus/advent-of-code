646d05be2fce483229c3ad5ebef97dbd77fc0659
asi0 <asi0.flammeus@pm.me>
2022-12-25 14:22:31 +0100

    -a
    update divisibility function

--- 2022/11/divisibility_rules.py
+++ 2022/11/divisibility_rules.py
@@ -18,12 +18,24 @@ def is_divisible(number, divisor):
         return is_divisible(number, 2) and is_divisible(number, 3)
     # Check for divisibility by 7
     elif divisor == 7:
+        tmp = number
+        while len(str(tmp)) > 5:
+            if tmp == 7:
+                return True
+            # Double the last digit of the number
+            last_digit = int(str(tmp)[-1])
+            # Subtract it from the rest of the number
+            rest_of_number = int(str(tmp)[:-1])
+            # Update tmp number
+            tmp = 5 * last_digit + rest_of_number
+
         # Double the last digit of the number
-        double_last_digit = int(str(number)[-1]) * 2
+        last_digit = int(str(tmp)[-1])
         # Subtract it from the rest of the number
-        rest_of_number = int(str(number)[:-1])
+        rest_of_number = int(str(tmp)[:-1])
         # Check if the result is divisible by 7
-        return (rest_of_number - double_last_digit) % 7 == 0
+
+        return (5 * last_digit + rest_of_number) % 7 == 0
     # Check for divisibility by 8
     elif divisor == 8:
         return int(str(number)[-3:]) % 8 == 0
@@ -43,6 +55,14 @@ def is_divisible(number, divisor):
         return is_divisible(number, 3) and is_divisible(number, 4)
     # Check for divisibility by 13
     elif divisor == 13:
+        if number == 13:
+            return True
+        # Double the last digit of the number
+        last_digit = int(str(number)[-1])
+        # Subtract it from the rest of the number
+        rest_of_number = int(str(number)[:-1])
+        # Check if the result is divisible by 7
+        return (4 * last_digit + rest_of_number) % 13 == 0
         digit_sum = sum(int(str(number)[i]) * (13 ** (len(str(number)) - i - 1)) for i in range(len(str(number))))
         return digit_sum % 13 == 0
     # Check for divisibility by 14
@@ -56,32 +76,85 @@ def is_divisible(number, divisor):
         return int(str(number)[-4:]) % 16 == 0
     # Check for divisibility by 17
     elif divisor == 17:
-        alternating_sum = sum(int(str(number)[i]) * (17 ** (len(str(number)) - i - 1)) for i in range(len(str(number))))
-        return alternating_sum % 17
+        if number == 17:
+            return True
+        tmp = number
+        while len(str(tmp)) > 5:
+            # Double the last digit of the number
+            last_digit = int(str(tmp)[-1])
+            # Subtract it from the rest of the number
+            rest_of_number = int(str(tmp)[:-1])
+            # Update tmp number
+            tmp = rest_of_number - 5 * last_digit
+
+        # Double the last digit of the number
+        last_digit = int(str(tmp)[-1])
+        # Subtract it from the rest of the number
+        rest_of_number = int(str(tmp)[:-1])
+
+        # Check if the result is divisible by 7
+        return (rest_of_number - 5 * last_digit) % 17 == 0
+
     # Check for divisibility by 18
     elif divisor == 18:
         return is_divisible(number, 2) and is_divisible(number, 9)
     # Check for divisibility for 19
     elif divisor == 19:
-        # Add up the digits of the number
-        digit_sum = sum(int(digit) for digit in str(number))
-        # Check if the sum is divisible by 19
-        return digit_sum % 19 == 0
+        if number == 19:
+            return True
+
+        tmp = number
+        while len(str(tmp)) > 5:
+            if tmp == 19:
+                return True
+            # Double the last digit of the number
+            last_digit = int(str(tmp)[-1])
+            # Subtract it from the rest of the number
+            rest_of_number = int(str(tmp)[:-1])
+            # Update tmp number
+            tmp = 2 * last_digit + rest_of_number
+
+        # Double the last digit of the number
+        last_digit = int(str(tmp)[-1])
+        # Subtract it from the rest of the number
+        rest_of_number = int(str(tmp)[:-1])
+
+        # Check if the result is divisible by 7
+        return (2 * last_digit + rest_of_number) % 19 == 0
+
     # Check for divisibility by 20
     elif divisor == 20:
         return is_divisible(number, 4) and is_divisible(number, 5)
     # Check for divisibility by 21
     elif divisor == 21:
+        print(is_divisible(number,3), is_divisible(number,7))
         return is_divisible(number, 3) and is_divisible(number, 7)
     # Check for divisibility by 22
     elif divisor == 22:
         return is_divisible(number, 11) and is_divisible(number, 2)
     # Check for divisibility by 23
     elif divisor == 23:
-        # Multiply each digit of the number by the corresponding power of 23, starting from the right
-        weighted_sum = sum(int(digit) * (23 ** i) for i, digit in enumerate(reversed(str(number))))
-        # Check if the sum is divisible by 23
-        return weighted_sum % 23 == 0
+        if number == 23:
+            return True
 
-for i in range(24):
-    print(i,is_divisible(i,i))
+        tmp = number
+        while len(str(tmp)) > 5:
+            if tmp == 23:
+                return True
+            # Double the last digit of the number
+            last_digit = int(str(tmp)[-1])
+            # Subtract it from the rest of the number
+            rest_of_number = int(str(tmp)[:-1])
+            # Update tmp number
+            tmp = 7 * last_digit + rest_of_number
+
+        # Double the last digit of the number
+        last_digit = int(str(tmp)[-1])
+        # Subtract it from the rest of the number
+        rest_of_number = int(str(tmp)[:-1])
+
+        # Check if the result is divisible by 7
+        return (7 * last_digit + rest_of_number) % 23 == 0
+    else:
+        return number%divisor == 0
+
