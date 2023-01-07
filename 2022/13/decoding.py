790d3c6021b3fb0b8d45b824720fc63fbb1c7dba
asi0 <asi0.flammeus@pm.me>
2023-01-07 09:56:32 +0100

    implementing get_core_list

--- 2022/13/decoding.py
+++ 2022/13/decoding.py
@@ -10,23 +10,47 @@ def check_order(left, right):
             return False
         else:
             return check_order(left[1:], right[1:])
-    elif isinstance(left, list) and isinstance(right, list):
-        print(left[0], right[0])
-        if left[0] < right[0]:
-            return True
-        elif right[0] < left[0]:
-            return False
-        else:
-            return check_order(left[1:], right[1:])
+
     elif isinstance(left, int) and isinstance(right, list):
-        return check_order([left] + left[1:], right)
+        while isinstance(right, list): # peeling the left packet
+            right = right[0]
+        return check_order(left, right)
+
     elif isinstance(left, list) and isinstance(right, int):
-        print([right]+right[1:])
-        return check_order(left, [right] + right[1:])
+        while isinstance(left, list): # peeling the right packet
+            left = left[0]
+        return check_order(left, right)
+
+    elif isinstance(left, list) and isinstance(right, list):
+        while isinstance(left, list) and isinstance(right, list): # peeling both packet
+            left = left[0]
+            right = right[0]
+        return check_order(left, right)
     else:
-        return False
+        return "something's wrong"
 
 """ modifier l'instruction elif pour faire un test à left[0] et right[0]
     pour vérifier si un des deux est une liste.
     Auquel cas retourner il faut continuer à décortiquer la list (comme pour les 2 prochains elif)
 """
+
+def get_core_list(lst):
+    if not isinstance(lst, list):
+        return lst
+    elif len(lst) == 1:
+        return get_core_list(lst[0])
+    elif len(lst) > 1:
+        return lst
+    else:
+        return None
+
+# test the function
+lst = [[[0,[[1,[[1, 2, 3]]]]]]]
+print(get_core_list(lst))
+lst = [[[[[[[]]]]]]]
+print(get_core_list(lst))
+lst = [[[[[[[], []]]]]]]
+print(get_core_list(lst))
+
+lst = 1
+print(get_core_list(lst))
