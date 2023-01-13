744ae9e676213090a0d07ab5c9471b5bea273d3d
asi0 <asi0.flammeus@pm.me>
2023-01-13 14:27:13 +0100

    __init__ implemented for Code object

--- 2022/13/decode.py
+++ 2022/13/decode.py
@@ -1,47 +1,44 @@
 class Code:
-    def __init__(self, input_list):
+    def __init__(self, input_txt):
         self.intergers = {}
-        for i, element in enumerate(input_list):
-            self.intergers[i] = {"value": element,
-                                "address": (0,0)
-                                 }
+        self.depth = 0
+        self.list_index = 0
+        self.dict_index = 0
+        self.traverse_code(input_txt)
 
+    def traverse_code(self, nested_list):
+        """ parse a nested list to extract the address of
+            each intergers.
+            an address is define by a depth and and list_index
+        """
+        if isinstance(nested_list, int):
+            interger = nested_list
+            self.intergers[self.dict_index] = {
+                                                "value": interger,
+                                                "depth": self.depth,
+                                                "index": self.list_index
+                                            }
+            self.dict_index += 1
+        elif len(nested_list) == 0:
+            interger = -1 # -1 is associated to an empty set
+            self.intergers[self.dict_index] = {
+                                                "value": interger,
+                                                "depth": self.depth,
+                                                "index": self.list_index
+                                            }
+            self.dict_index += 1
+        else:
+            self.depth += 1
+            for i, element in enumerate(nested_list):
+                self.list_index = i
+                self.traverse_code(element)
 
 a = [[1],[2,3,4]]
 
 code_test = Code(a)
+print(a)
 print(code_test.intergers)
 
-current_adrress = [0]
-a_adresses = []
-for i, element in enumerate(a):
-    if isinstance(element, int) or element == None:
-        a_adresses.append(current_adrress)
-    else:
-        a = 1
-b = [[[]]]
-depth = 0
-index = 0
-def traverse(nested_list, depth, index):
-    global addresses
 
-    if isinstance(nested_list, int):
-        interger = nested_list
-        addresses.append([interger, depth, index])
-
-    elif len(nested_list) == 0:
-        empty_list = nested_list
-        addresses.append([empty_list, depth, index])
-    else:
-        depth += 1
-        for i, element in enumerate(nested_list):
-            traverse(element, depth, i)
-
-c = [1,[2,[3,[4,[5,6,7]]]],8,9]
-addresses = []
-print(traverse(c, depth, index))
-print()
-print(addresses)
-
-""" need to add the traverse function into the Code class into order to only change addresses dict of the corresponding code instance.
+""" next step try to find a way to check value between 2 different codes
 """
