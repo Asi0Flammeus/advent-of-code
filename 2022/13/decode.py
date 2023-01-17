5b5232240cf50bebf62d97ab24cab39737fa03f7
asi0 <asi0.flammeus@pm.me>
2023-01-17 08:56:13 +0100

    implementing check_procedure

--- 2022/13/decode.py
+++ 2022/13/decode.py
@@ -33,12 +33,29 @@ class Code:
                 self.list_index = i
                 self.traverse_code(element)
 
-a = [[1],[2,3,4]]
+a = [[],[1,[3,[0]]],[]]
+b = [[],[[0,[5,3,0,1,0],[3,0,5,7],10,[2,8,5,0]],10,[2,4,[1],[5,6,7],[]],[]]]
 
-code_test = Code(a)
+left = Code(a)
+right = Code(b)
+
 print(a)
-print(code_test.intergers)
+print(left.intergers)
+print(left.intergers.get(25,None) is None)
 
-
+print()
+print(b)
+print(right.intergers)
 """ next step try to find a way to check value between 2 different codes
 """
+
+def check_procedure(left, right):
+    left_depth = 1
+    right_depth = 1
+
+    for index in range(len(left.intergers)):
+        if right.intergers.get(index,None) is None:
+            return False
+        else:
+            # how to parse each element to verify left < right?
+            pass
